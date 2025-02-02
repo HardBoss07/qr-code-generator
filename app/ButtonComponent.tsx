ababdc4c4364deb73db4cb9be64be350e69a7bc1
HardBoss07 <matteo@bosshard.net>
2025-02-02 15:11:19 +0100

    updated ButtonComponent.tsx so that you have to define a label for the button

--- app/ButtonComponent.tsx
+++ app/ButtonComponent.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 type ButtonComponentProps = {
   onClick: () => void;
+  label: string;
 }
 
-const ButtonComponent: React.FC<ButtonComponentProps> = ({ onClick }) => {
+const ButtonComponent: React.FC<ButtonComponentProps>= ({ onClick, label }) => {
   return (
     <button
       onClick={onClick}
       className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
     >
-      Clear QR Code
+      {label}
     </button>
   );
 };
